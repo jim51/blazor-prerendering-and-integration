82e49cccc1b2213aac8be97e00fe060464bb6813
Jim <kstsabc@gmail.com>
2022-04-29 15:36:31 +0800

    Add project files.

---
+++ blazor-prerendering-and-integration/Shared/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace blazor_prerendering_and_integration.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
